cf431850d07b75e4e25a55c050df2ec7755797aa
IT-AKYLA <mihailbwm@gmail.com>
2022-07-24 23:59:22 +0300

    menu!

--- homework(27.07.22).py
+++ homework(27.07.22).py
@@ -2,8 +2,55 @@ import json
 import pandas
 
 
-def Json_to_csv (json_object):  # 1 json to csv
-    return pandas.DataFrame(json_object).to_csv('first.assigment.csv')
+def Language_search (sl):
+    st = input('Select a language: ')
+    for dicts in sl:
+        for key, value in dicts.items():  # а без функции работает!
+            if key == 'languages' and st in value:
+                print([i for i in {k: v for k, v in dicts.items() if k == 'name'}.values()])
+
+    Online()
+
+
+def Research_name(data, name = ''):
+    for dicts in data:
+        for key, value in dicts.items():
+            if key == 'name' and value == name:
+                return { k : v for k, v in dicts.items() }
+    else:
+        return 'Person not found!'
+
+
+def Name_search ():
+    with open('first_assignment.json', 'r') as f:
+        print(f"Search result: {Research_name(json.load(f), input('input name: ').title())}")
+    Online()
+
+
+def Add (sl):
+    sl_add = {
+        "name": input('input Name: ').title(),
+        "birthday": input('input your birthday: '),
+        "height": input('input your height: '),
+        "weight": input('input your  weight: '),
+        "car": True if input('Do you have a car: ') == 'yes' else False,
+        "languages": [input('What languages do you know: ').title() for i in
+                      range(0, int(input('How many languages do you know?: ')))]
+
+    }
+    print('\n')
+    sl.append(sl_add)
+
+    with open('first_assignment.json', 'w') as f:
+        json.dump(sl, f, indent=3)
+
+    Online()
+
+
+def Json_to_csv(sl):  # 1 json to csv
+    pandas.DataFrame(sl).to_csv('first.assigment.csv')
+    print('Executed!!!\n')
+
 
 def Online():
     print('Shall we continue(yes/no)?')
@@ -13,13 +60,25 @@ def Online():
         print(end= '\n')
         pass
 
-def Research_name(data, name):
-    for dicts in data:
-        for key, value in dicts.items():
-            if key == 'name' and value == name:
-                return { k : v for k, v in dicts.items() }
-    else:
-        return 'Person not found!'
+
+def Menu(sl):
+    print(f'1: Would you like to add your dates?:\n2: Do you want to find data by name(yes/no)?:\n3: Well look for people by language(yes/no)?:\n''4: Json_to_csv ')
+    choice = input('Make a choice(1, 2, 3, 4 or exit! ): ')
+
+    if choice == '1':
+        Add(sl)
+
+    if choice == '2':
+        Research_name(sl)
+
+    if choice == '3':
+        Language_search(sl)
+
+    if choice == '4':
+        Json_to_csv(sl)
+
+    if choice == 'exit':
+        exit()
 
 # def Research_language(langs, lang):
 #     for dicts in langs:
@@ -29,76 +88,48 @@ def Research_name(data, name):
 #     else:
 #         return 'This language is not detected!!'
 
-while True:
 
-    data = """
-[
-    {
-        "name": "John Smith",
-        "birthday": "02.10.1990",
-        "height": 175,
-        "weight": 76.5,
-        "car": true,
-        "languages": ["C++", "Python"]
-    },
-    {
-        "name": "Alexey Alexeev",
-        "birthday": "05.06.1986",
-        "height": 197,
-        "weight": 101.2,
-        "car": false,
-        "languages": ["Pascal", "Delphi"]
-    },
-    {
-        "name": "Maria Ivanova",
-        "birthday": "28.08.1998",
-        "height": 165,
-        "weight": 56.1,
-        "car": true,
-        "languages": ["C#", "C++", "C"]
-    }
-]
-"""
-    sl = json.loads(data)
 
-    if input('Would you like to add your dates (yes/no/)?:  ') == 'yes':
-        sl_add = {
-            "name" : input('input Name: '),
-            "birthday" : input('input your birthday: '),
-            "height" : input('input your height: '),
-            "weight" : input('input your  weight: '),
-            "car" :  True if input('Do you have a car: ') == 'yes' else False,
-            "languages" : [input('What languages do you know: ').title() for i in range(0, int(input('How many languages do you know?: ')))]
+
+
+data = """
+    [
+        {
+            "name": "John Smith",
+            "birthday": "02.10.1990",
+            "height": 175,
+            "weight": 76.5,
+            "car": true,
+            "languages": ["C++", "Python"]
+        },
+        {
+            "name": "Alexey Alexeev",
+            "birthday": "05.06.1986",
+            "height": 197,
+            "weight": 101.2,
+            "car": false,
+            "languages": ["Pascal", "Delphi"]
+        },
+        {
+            "name": "Maria Ivanova",
+            "birthday": "28.08.1998",
+            "height": 165,
+            "weight": 56.1,
+            "car": true,
+            "languages": ["C#", "C++", "C"]
         }
-        sl.append(sl_add)
-        Online()
-    else:
-        Online()
-        pass
+    ]
+    """
+sl = json.loads(data)
+with open('first_assignment.json', 'w') as file:
+    json.dump(sl, file, indent=3)
 
-    with open('first_assignment.json', 'w') as file:
-        json.dump(sl, file, indent= 3)
+while True:
+    Menu(sl)
 
-    Json_to_csv(pandas.read_json('first_assignment.json'))
 
-    if input("Do you want to find data by name(yes/no)?: ") == 'yes':
-        with open('first_assignment.json','r') as f:
-            print(f"Search result: {Research_name(json.load(f), input('input name: ').title())}")
-        Online()
-    else:
-        Online()
 
-    if input("We'll look for people by language(yes/no)?: ") == 'yes':
-        st = input('Select a language: ')
-        for dicts in sl:
-            for key, value in dicts.items():                                                            #а без функции работает!
-                if key == 'languages' and st in value:
-                    print([i for i in {k: v for k, v in dicts.items() if k == 'name'}.values()])
 
-        Online()
-
-    else:
-        Online()
 
 
 
